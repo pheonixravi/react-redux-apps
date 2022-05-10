38a46d0ef79200f4e121c864f1b8d16db2a673ac
LAPTOP-052SRALO\ravik <ravikumar.krishnamurthy@gmail.com>
2022-05-10 18:08:45 +0530

    redux with RTK

---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Steve Jobs",
+    username: "Steve",
+  },
+  {
+    id: 2,
+    name: "Sathya Nadella",
+    username: "Sathya",
+  },
+  {
+    id: 3,
+    name: "Sundar Pichai",
+    username: "sundar",
+  },
+  {
+    id: 4,
+    name: "James Gosling",
+    username: "james",
+  },
+  {
+    id: 5,
+    name: "Shiddu",
+    username: "Shiddu@smk",
+  },
+  {
+    id: 6,
+    name: "Jake Runolfsdottir V",
+    username: "Leopoldo_Corkery",
+  },
+  {
+    id: 7,
+    name: "Mrs. Dennis Schulist",
+    username: "Karley_Dach",
+  },
+  {
+    id: 8,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+  },
+];
